dcdcc989ca2cedf9e08f4074b7b940ff3428af52
honghong1234 <yzihong307@gmail.com>
2019-09-15 11:49:22 +0800

    完成抓包数据解析

--- capture.py
+++ capture.py
@@ -1,7 +1,8 @@
 from scapy.all import *
-import socket
 
-dpkt = sniff(filter='src dst 192.168.31.124', count=10)
+dpkt = sniff(filter='src dst 192.168.9.254', count=10)
 wrpcap("demo.pcap", dpkt)
+
 pcap_path = "demo.pcap"  # 用于解析文件的路径
 pcap_saved = sniff(offline=pcap_path)  # 将保存下来的pcap再次实例化为python对象
+
--- pcap.py
+++ pcap.py
@@ -1,24 +1,17 @@
+import dpkt
 from scapy.all import *
-from scapy.utils import PcapReader
-import socket
-import io
-import struct
 
 
-def analysis(pcap_path):
-    file = open(pcap_path, 'rb')
-    file_length = int(file.seek(0, io.SEEK_END))
-    file.seek(24)
-    header = 24
-    tcp_stream = []
-    while header < file_length:
-        file.seek(8, io.SEEK_CUR)
-        pkt_length = struct.unpack('I', file.read(4))[0]
-        file.seek(4, io.SEEK_CUR)
-        pkt_body = file.read(pkt_length)
-        tcp_stream.append(pkt_body)
-        header += pkt_body + 16
+def DecodePcapPackage(pcapFileName):
+    pcaps = rdpcap(pcapFileName)
+    for pcap in pcaps:
+        print(pcap.show())
+        print("\n\n\n")
+
+
+def main():
+    DecodePcapPackage("demo.pcap")
 
 
 if __name__ == '__main__':
-    analysis('pc.pcap')
+    main()
